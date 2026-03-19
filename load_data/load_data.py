48dfac56b5ad2e1fb856802055563c37d688e3e6
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2026-03-19 15:08:46 +0100

    Added configuration for training

--- load_data/load_data.py
+++ load_data/load_data.py
@@ -7,9 +7,22 @@ raw_path     = config["dataset"]["location"]
 cleaned_path = config["dataset"]["cleaned"]  
 test_size    = config["model"]["test_size"]  
 random_state = config["model"]["random_state"] 
+target_class = config["target"]["name"]
 
 def load_data():
     return pd.read_csv(raw_path)
+
+def load_cleaned_data():
+    return pd.read_csv(cleaned_path)
+
+def load_class():
+    return str(target_class)
+
+def test_size_load():
+    return int(test_size)
+
+def random_state_load():
+    return int(random_state)
 
 
 
