67eb93c179d7ddd09013e0833affdd5819233c90
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2026-03-23 15:25:41 +0100

    Added api

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from preprocessing.preprocess import preprocess_data
 from train.train_model import train_model
+import uvicorn
 
 if __name__ == "__main__":
     preprocess_data()
     train_model()
+    uvicorn.run("api.api:app", host="0.0.0.0", port=8000, reload=False)
